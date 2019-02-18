aef6a4fa5ce57e954f5d46f0256aef78313ee5d1
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-02-18 15:55:29 +0000

    Describe mangling in help, update tests for wording changes

--- src/cli/generate/bundle/options/Bundleid.option.ts
+++ src/cli/generate/bundle/options/Bundleid.option.ts
@@ -21,6 +21,8 @@ export const BundleidOption: ICommandOptionDefinition = {
     type: "string",
     description: "The ID for the generated CICS bundle, up to 64 characters. If no value is " +
                  "specified then a default value is created from the 'name' property " +
-                 "in the package.json file in the current working directory."
+                 "in the package.json file in the current working directory." +
+                 "If the value is too long it will be truncated. If it contains characters " +
+                 "not supported by CICS, each one will be replaced by an X."
 };
 
--- src/cli/generate/bundle/options/Nodejsapp.option.ts
+++ src/cli/generate/bundle/options/Nodejsapp.option.ts
@@ -21,6 +21,9 @@ export const NodejsappOption: ICommandOptionDefinition = {
     type: "string",
     description: "The ID of the generated CICS NODEJSAPP resource, up to 32 characters.  If no value is " +
                  "specified then a default value is created from the " +
-                 "'name' property in package.json, or the bundleid option if specified. "
+                 "'name' property in package.json, or the bundleid option if specified. " +
+                 "If the value is too long it will be truncated. If it contains characters " +
+                 "not supported by CICS, each one will be replaced by an X."
+
 };
 
