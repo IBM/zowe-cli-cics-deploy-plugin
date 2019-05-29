91a798d6fd250f4627c6db48d6f2e435e4ebbaf9
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-05-29 15:06:47 +0100

    Reinstate line

--- src/imperative.ts
+++ src/imperative.ts
@@ -15,6 +15,7 @@ import {IImperativeConfig} from "@zowe/imperative";
 const MAX_LENGTH = 8;
 const MAX_HLQ_LENGTH = 35;
 const MAX_TARGETDIR_LENGTH = 255;
+
 const config: IImperativeConfig = {
     commandModuleGlobs: ["**/cli/*/*.definition!(.d).*s"],
     pluginHealthCheck: __dirname + "/healthCheck.handler",
