d2aacb45ebfa52dea3bc6770d18f79e6937894ec
Matthew Wilson <matthew.wilson@uk.ibm.com>
2019-05-31 10:15:52 +0100

    Add copyright statements

--- __tests__/api/BundlePush/SubtaskWithStatus.test.ts
+++ __tests__/api/BundlePush/SubtaskWithStatus.test.ts
@@ -1,3 +1,13 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright IBM Corp, 2019
+*
+*/
 import { SubtaskWithStatus } from "../../../src/api/BundlePush/SubtaskWithStatus";
 import { TaskStage, ITaskWithStatus, TaskProgress } from "@zowe/imperative";
 
